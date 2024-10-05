3f032acc0e4b82d5303813fc03d42bef016d3573
Abhinav Gupta <abhinavgupta1882002@gmail.com>
2024-10-04 20:46:55 -0400

    changing for deployment

--- server.py
+++ server.py
@@ -78,4 +78,5 @@ def process_request():
     }
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
